4410369e78822a6585d1086031f20e262ff3df2a
WeThePixies <30452154+WeThePixies@users.noreply.github.com>
2018-05-12 08:57:37 -0700

    V1.2

--- offline.js
+++ offline.js
@@ -1,10 +1,13 @@
 ﻿{
-	"version": 1501124339,
+	"version": 1526140522,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
+		"offlineClient.js",
 		"images/1a99f84015ae471d82e55c5658cf-sheet0.png",
+		"images/icon-sheet0.png",
+		"images/icon2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
@@ -14,6 +17,9 @@
 		"c2mp-net.js",
 		"c2mp-peer.js",
 		"c2mp.js",
-		"waker.js"
+		"waker.js",
+		"sweetalert2.min.css",
+		"sweetalert2.min.js",
+		"es6-promise.js"
 	]
 }
